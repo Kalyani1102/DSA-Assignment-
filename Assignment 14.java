9cd27fccc5a26c6e42b2c0374f74f84dc6808f0b
Kalyani1102 <94071212+Kalyani1102@users.noreply.github.com>
2022-03-11 18:50:01 +0530

    Create Assignment 14.java

---
+++ Assignment 14.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Assignment_14 {
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int arr[][] = new int[m][n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
