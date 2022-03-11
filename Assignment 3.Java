5f3e9a8251a4631445b353ac3c9229c37b38a073
Kalyani1102 <94071212+Kalyani1102@users.noreply.github.com>
2022-03-11 18:41:17 +0530

    Create Assignment 3.Java

---
+++ Assignment 3.Java
@@ -0,0 +1,37 @@
+package Solution;
+
+import java.util.Scanner;
+
+public class Assignment_3 {
+    static int binary(int[] arr, int target) {
+        int result = -1;
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (target > arr[mid]) {
+                low = mid + 1;
+                result = low;
+            } else {
+                high = mid - 1;
+                result = high + 1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+
+        int result = binary(arr, target);
+        System.out.println(result);
+    }
+
+}
