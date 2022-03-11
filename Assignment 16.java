3851db8043269c3d4f0f4c0780ad78a155a3d15e
Kalyani1102 <94071212+Kalyani1102@users.noreply.github.com>
2022-03-11 18:52:17 +0530

    Create Assignment 16.java

---
+++ Assignment 16.java
@@ -0,0 +1,31 @@
+import java.util.*;
+ 
+class Assignment_16 {
+ 
+    
+    public static int findMinRec(int A[], int n)
+    {
+    
+      if(n == 1)
+        return A[0];
+         
+        return Math.min(A[n-1], findMinRec(A, n-1));
+    }
+     
+    public static int findMaxRec(int A[], int n)
+    {
+      if(n == 1)
+        return A[0];
+         
+        return Math.max(A[n-1], findMaxRec(A, n-1));
+    }
+ 
+    
+    public static void main(String args[])
+    {
+        int A[] = {1, 4, 45, 6, -50, 10, 2};
+        int n = A.length;
+        System.out.println(findMinRec(A, n));
+        System.out.println(findMaxRec(A , n));
+    }
+}
