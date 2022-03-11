670f241c213c39a1f8911174c030a44f69c8562e
Kalyani1102 <94071212+Kalyani1102@users.noreply.github.com>
2022-03-11 18:43:48 +0530

    Create Assignment 7.java

---
+++ Assignment 7.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Assignment_7 {
+    public static int[] two_sum(int[] arr, int target_sum) {
+        int[] arr1 = { -1, -1 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target_sum) {
+                    arr1[0] = i;
+                    arr1[1] = j;
+                }
+            }
+        }
+        return arr1;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target_sum = sc.nextInt();
+        int[] result = two_sum(arr, target_sum);
+        for (int k = 0; k < 2; k++) {
+            System.out.printf("%d ", result[k]);
+        }
+    }
+}
